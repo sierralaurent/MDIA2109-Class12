bf0203f7eda4deea6723bb48ad764572e6a8dff6
Sierra Laurent <sierraclaurent@gmail.com>
2023-03-20 14:34:38 -0700

    first commit

---
+++ .history/components/BarChart/index_20230320135937.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import styles from './BarChart.module.css';
+
+import { Bar } from 'react-cartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
---
+++ README.md
@@ -0,0 +1,9 @@
+# Chart.js 2
+https://react-chartjs-2.js.org/ 
+
+## Dependancy Installation 
+```
+npm i react-chartjs-2
+```
+
+## BarChart
